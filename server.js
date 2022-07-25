16a668d5fec931d5f3ebbadf1cec28faa6a1cefb
Ashutosh Akkole <ashutosh.akkole19@vit.edu>
2022-07-26 00:27:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.set("view engine","ejs");
+
+
+app.get("/",function(req,res){
+    res.render("home",{});
+})
+
+
+app.listen(3000,function(){
+    console.log("blog server started on port 3000");
+});
